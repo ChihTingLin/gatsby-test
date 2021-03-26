16cad94726c969c840ce75a0b41962dd83907d50
Chih-Ting Lin <chihtinglin03@gmail.com>
2021-03-26 23:17:00 +0800

    Modify index.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
 module.exports = {
+    pathPrefix: "/gatsby-test",
     plugins: [
-
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
+        // `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+              name: `images`,
+              path: `${__dirname}/src/images/`,
+            },
+          }
     ]
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Layout.module.css";
+import { StaticImage } from "gatsby-plugin-image";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <div className={styles.header}>
+        <StaticImage
+          width={30}
+          height={30}
+          src="../images/icon.png"
+          alt="logo"
+        />
+      </div>
+      {children}
+      <div className={styles.footer}></div>
+    </>
+  );
+}
